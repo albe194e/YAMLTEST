8a3df54481fe3dfc4216e63a96f9661244769545
albe194e <albe194e@stud.kea.dk>
2023-02-08 10:22:44 +0100

    Initial commit

---
+++ src/main/java/com/example/yamltest/YamltestApplication.java
@@ -0,0 +1,13 @@
+package com.example.yamltest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YamltestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YamltestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/yamltest/YamltestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.yamltest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YamltestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
